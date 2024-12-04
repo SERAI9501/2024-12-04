5bec850ab5e6220bb244939f078a04596ffc6c2c
sist102 <sist102@DESKTOP-06VP6JK>
2024-12-04 19:19:11 +0900

    1

--- 2024-12-03/src/아침복습.java
+++ 2024-12-03/src/아침복습.java
@@ -183,15 +183,15 @@ public class 아침복습 {
 //			}
 //		}
 //
-		int[] arr= {10,20,30,50,3,60,-3};
-		for(int i=0;i<arr.length;i++)
-		{
-			if(arr[i]==60)
-			{
-				System.out.println(arr[i]);
-				break;
-			}
-		}
+//		int[] arr= {10,20,30,50,3,60,-3};
+//		for(int i=0;i<arr.length;i++)
+//		{
+//			if(arr[i]==60)
+//			{
+//				System.out.println(arr[i]);
+//				break;
+//			}
+//		}
 			
 			}
 		
