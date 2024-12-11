6679939ffc90cafb71be819b79af47fa8301aeac
sist102 <sist102@DESKTOP-06VP6JK>
2024-12-12 08:44:00 +0900

    1

--- Chapter05_객체지향프로그램_6/src/com/sist/main/접근지정어_문제.java
+++ Chapter05_객체지향프로그램_6/src/com/sist/main/접근지정어_문제.java
@@ -150,8 +150,7 @@ public class 접근지정어_문제 {
 //				    
 //				    public void setName(String name) { } // ---------------------(  (3)  ) 메소드
 //				}
-//				Colored by Color Scripter
-//				cs
+//				Colored by Color Scriptercs
 //
 //				11. 현실 세계의 회원을 Member 클래스로 모델링하려고 합니다. 회원의 데이터로는 이름, 아이디, 패스워드, 나이가 있습니다. 이 데이터들을 가지는 Member 클래스를 선언해보세요.
 //				 데이터 이름	 필드 이름 	 타입 
